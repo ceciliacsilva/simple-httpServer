ded6a80b353df6916552d7c7782ae76f466aa08e
Cecília Carneiro e Silva <cecilia.carneiroesilva@gmail.com>
2016-10-22 13:21:10 -0200

    com dispatch

---
+++ readme.md
@@ -0,0 +1,7 @@
+http://thenewstack.io/building-a-web-server-in-go/
+
+https://tutorialedge.net/creating-a-simple-web-server-with-go-golang
+
+https://golang.org/src/net/http/request.go?h=Header%28%29.Set
+
+https://astaxie.gitbooks.io/build-web-application-with-golang/content/en/03.2.html
--- server.go
+++ server.go
@@ -1,9 +1,42 @@
+/*
+|**********************************************************************;
+* Projeto           : Simple HTTP web-server in Golang.
+*
+* Program name      : server.go
+*
+* Author            : Cecília Carneiro e Silva
+*
+* Date created      : 22/10/2016
+*
+* Purpose           : Server HTTP/1.0 using Golang, net/http.
+*
+* Uso               : $ go run server.go
+*
+|**********************************************************************;
+*/
+
 package main
 
 import (
 	"fmt"
         "net/http"
 )
+
+//like hash table
+var dispatch map[string]func(http.ResponseWriter, *http.Request)
+
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	var path string
+	path = r.URL.Path[1:]
+	if h, ok := dispatch[path]; ok{
+		h(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "oi mundo")
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
@@ -15,6 +48,14 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	server := http.Server{
+		Addr:    ":8080",
+		Handler:  &myHandler{},
+	}
+
+	dispatch = make(map[string]func(http.ResponseWriter, *http.Request))
+
+	dispatch["/cecilia"] = handler
+
+	server.ListenAndServe()
 }
